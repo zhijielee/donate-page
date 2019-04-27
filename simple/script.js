94894f9be5e70ac7ce4cd92e9d88f0a9342f18c5
Die antwort <46244674+zhijielee@users.noreply.github.com>
2019-04-27 23:43:53 +0800

    Update script.js

--- simple/script.js
+++ simple/script.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
 	var QRBox	=	$('#QRBox');
 	var MainBox	=	$('#MainBox');
-	var AliPayQR	=	'images/AliPayQR.png';
-	var WeChanQR	=	'images/WeChanQR.png';
+	var AliPayQR	=	'images/aliPay.jpg';
+	var WeChanQR	=	'images/WeChatPay.png';
 
 	
 
